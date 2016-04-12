96972646d71ff7c1d8261cb41e06526ae097881a
silverbux <alexquiambao@gmail.com>
2016-04-12 00:21:36 +0000

    test

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -2,6 +2,6 @@ package org.eclipse.che.examples;
 
 public class HelloWorld {
     public static void main(String[] argvs) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World3!");
     }
 }
